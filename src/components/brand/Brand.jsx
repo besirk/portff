c0f38e46713049eb4c2fe848788d692c06f62121
Besir Kurtishi <kurtishi45@gmail.com>
2022-07-24 22:49:44 +0200

    framer motion included

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import './brand.css';
 import Particle from '../../components/particle';
+import {motion} from 'framer-motion';
 
 const Brand = () => {
   return (
    <>
    
-    <div className='modern__brand section__padding' id='skills'>
+    <motion.div className='modern__brand section__padding' id='skills' 
+      initial = {{opacity:0}}
+      whileInView={{ opacity: 1 }}
+      transition={{ delay: 0.2 }}
+    >
       <Particle />
-      <div className='modern__brand-content'>
+      <motion.div className='modern__brand-content' 
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{delay:0.5}}
+      >
       <h1>Skills</h1>
-      <ul>
+      <motion.ul
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{delay:0.8}}
+        >
         <li>HTML</li>
         <li>CSS</li>
         <li>Javascript</li>
         <li>ReactJs</li>
-      </ul>
-      </div>
-      </div>
+      </motion.ul>
+      </motion.div>
+      </motion.div>
     </>
   )
 }
